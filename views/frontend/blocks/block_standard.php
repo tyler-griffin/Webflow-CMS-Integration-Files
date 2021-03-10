977e37fbe1402a9970035009009327ad442eccbc
tyler-griffin <tyler.michael.griffin@gmail.com>
2021-03-10 10:45:43 -0600

    Update block_standard.php

--- views/frontend/blocks/block_standard.php
+++ views/frontend/blocks/block_standard.php
@@ -1,4 +1,4 @@
-<div class="text-block" data-id="<?= $block->id ?>">
+<div <? if(mb_strlen(strip_tags($block->html))>2) { ?>class="text-block"<? } ?> data-id="<?= $block->id ?>">
 
 	<?= $block->html ?>
 
