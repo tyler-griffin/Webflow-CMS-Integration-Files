04b9abc53826ff1959d32608a7ff14a27b6590a1
tyler-griffin <tyler.michael.griffin@gmail.com>
2022-01-12 12:14:42 -0600

    Added constants.php and globals.php

---
+++ config/constants.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+------------------------------------------------------------------
+  Global Constants
+------------------------------------------------------------------
+
+Define constants here that will be available anywhere in the
+application.  (Controllers, Models, Helpers, Libraries, Views)
+
+This file is loaded before any CodeIgniter Framework code, so
+you cannot use library functions or custom functions here.
+
+Use this file to define static paths and variables, but for
+dynamic variables, use "/site/config/globals.php"
+
+*/
+
+// This is used in the "bustCache()" function to version assets
+define("ASSETS_VERSION", "1.0.0");
+
+/*
+define(ALERT_BAR_BLOCK_ID, 3);
---
+++ config/globals.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+------------------------------------------------------------------
+  Global Variables
+------------------------------------------------------------------
+
+Define variables here that will be accessible in all views.
+
+This file is loaded within the scope of the CodeIgniter Framework
+as well as Editor Framework.
+
+To use these variables inside of a helper function, use:
+
+include(GLOBALS);
+
+*/
+
+$ci=& get_instance();
+
+/*
+------------------------------------------------------------------
+  Custom Global Variables
+------------------------------------------------------------------
+*/
+
+/*
+$DEV_CONFIG = strings(1);
+$COMMON_ITEMS = strings(2);
