4dbc8975c4381f9504705170e6f7f4ed39eed642
tyler-griffin <tyler.michael.griffin@gmail.com>
2023-12-22 16:53:55 -0600

    fix

--- views/frontend/inner.php
+++ views/frontend/inner.php
@@ -13,10 +13,10 @@ if(isset($page->settings["Subtitle"])) {
 } ?>
 
 <? // Create variable for interior banner image
-if(isset($COMMON_ITEMS["Default Page Banner"])) {
-	$interiorBanner = json_decode($COMMON_ITEMS["Default Page Banner"]);
-} else if(isset($page->settings["Page Banner"])) {
+if(isset($page->settings["Page Banner"])) {
 	$interiorBanner = json_decode($page->settings["Page Banner"]);
+} else if(isset($COMMON_ITEMS["Default Page Banner"])) {
+	$interiorBanner = json_decode($COMMON_ITEMS["Default Page Banner"]);
 } ?>
 
 <div class="title-section" <? if(isset($interiorBanner)) { ?>style="background-position: <?= $interiorBanner->config->{'background-position'} ?>; background-image: url('/image/<?= $interiorBanner->id ?>/2000');"<? } ?>>
