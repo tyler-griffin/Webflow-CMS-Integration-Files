55dbbb8b37a982ca514d5aa676e265fc81690272
tyler-griffin <tyler.michael.griffin@gmail.com>
2024-01-16 17:57:42 -0600

    added site and upload folders

--- _devtools/database-add-custom-tables.sql
+++ _devtools/database-add-custom-tables.sql
@@ -11,6 +11,30 @@ ALTER TABLE `amsd_standard` ADD `focused_img` text DEFAULT NULL;
 ALTER TABLE `amsd_standard_with_categories` DROP COLUMN IF EXISTS `img`;
 ALTER TABLE `amsd_standard_with_categories` ADD `focused_img` text DEFAULT NULL;
 
+UPDATE `core_developer_settings` SET `value`='/assets/css/cms.css' WHERE `setting`='contentsCSS';
+UPDATE `core_developer_settings` SET `value`='1' WHERE `setting`='stylesMenuV2';
+UPDATE `core_developer_settings` SET `value`='[{
+    \"title\": \"Heading 1\",
+    \"class\": \"heading\",
+    \"element\": \"div\"  
+},{
+    \"title\": \"Heading 2\",
+    \"class\": \"heading heading-2\",
+    \"element\": \"div\"  
+},{
+    \"title\": \"Heading 3\",
+    \"class\": \"heading heading-3\",
+    \"element\": \"div\"  
+},{
+    \"title\": \"Divider\",
+    \"class\": \"text-divider\",
+    \"element\": \"div\"  
+},{
+    \"title\": \"Call Out Text\",
+    \"class\": \"call-out-text\",
+    \"element\": \"div\"
+}]' WHERE `setting`='ckeditorStyles';
+
 
 CREATE TABLE IF NOT EXISTS `amsd_alert_bar` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
