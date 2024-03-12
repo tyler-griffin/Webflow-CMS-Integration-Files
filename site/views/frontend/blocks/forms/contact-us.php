8bb7ba40ac4b41f68669edf9c8ebb6ec92c1c374
tyler-griffin <tyler.michael.griffin@gmail.com>
2024-03-12 16:46:27 -0500

    Update contact-us.php

--- site/views/frontend/blocks/forms/contact-us.php
+++ site/views/frontend/blocks/forms/contact-us.php
@@ -3,17 +3,22 @@
 
 		<form data-module="email-form" class="form" data-block="<?= $block->id ?>">
 
-			<div class="form-input third">
-				<label class="floating-form-label" for="Name">Name</label>
-				<input class="input-field w-input" data-name="true" id="Name" maxlength="256" name="Name" data-required type="text">
+			<div class="form-input half">
+				<label class="floating-form-label" for="First_Name">First Name</label>
+				<input class="input-field w-input" data-name="true" id="First_Name" maxlength="256" name="First_Name" data-required type="text">
 			</div>
 
-			<div class="form-input third">
+			<div class="form-input half">
+				<label class="floating-form-label" for="Last_Name">Last Name</label>
+				<input class="input-field w-input" data-name="true" id="Last_Name" maxlength="256" name="Last_Name" data-required type="text">
+			</div>
+
+			<div class="form-input half">
 				<label class="floating-form-label" for="Email">Email</label>
 				<input class="input-field w-input" id="Email" maxlength="256" name="Email" data-required type="text" data-email="true">
 			</div>
 
-			<div class="form-input third">
+			<div class="form-input half">
 				<label class="floating-form-label" for="Phone">Phone</label>
 				<input class="input-field w-input" id="Phone" maxlength="256" name="Phone" data-required type="text" data-mask="(999) 999-9999">
 			</div>
@@ -23,7 +28,7 @@
 			</div>
 
 			<div class="form-input">
-				<button class="cms-btn contact-form-submit w-button"><span class="button-icon"><i class="fas fa-paper-plane"></i></span> Submit</button>
+				<button class="cms-btn contact-form-submit w-button"><span class="button-icon"><i class="fas fa-paper-plane"></i></span>Submit</button>
 			</div>
 			
 			<span class="contact-form-loading-icon"></span>
