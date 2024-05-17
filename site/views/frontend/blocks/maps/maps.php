e6648b9186ed83b8b549f448c37ef290c270cc3d
tyler-griffin <tyler.michael.griffin@gmail.com>
2024-05-17 15:10:05 -0500

    Update maps.php

--- site/views/frontend/blocks/maps/maps.php
+++ site/views/frontend/blocks/maps/maps.php
@@ -1,18 +1,26 @@
-<div data-id="<?= $block->id ?>" class="map">
+<div class="map-wrapper" data-id="<?= $block->id ?>">
 
-	<?
-	
-		$MAP_BLOCK = getBlock($block->id);
-		$address = $MAP_BLOCK["data"][0]->markers[0];
+	<div class="map">
+
+		<?
 		
-		if((isset($MAP_BLOCK["settings"]["Heading Read Only"]) && $MAP_BLOCK["settings"]["Heading Read Only"] == "true")) {} else { echo _titleTag($MAP_BLOCK["heading"]); }
+			$MAP_BLOCK = getBlock($block->id);
+			$address = $MAP_BLOCK["data"][0]->markers[0];
+			
+			if((isset($MAP_BLOCK["settings"]["Heading Read Only"]) && $MAP_BLOCK["settings"]["Heading Read Only"] == "true")) {} else { echo _titleTag($MAP_BLOCK["heading"]); }
 
-		$DIRECTIONS_URL = "https://www.google.com/maps?daddr=" . urlencode($address->address_1 . " " . $address->city . " " . $address->state . " " . $address->zip);
+			$DIRECTIONS_URL = "https://www.google.com/maps?daddr=" . urlencode($address->address_1 . " " . $address->city . " " . $address->state . " " . $address->zip);
+			
+		?>
+
+		<? displayMap($block->id); ?>
+
+		<? clr(); ?>
 		
-	?>
+	</div>
 
-	<? displayMap($block->id); ?>
+	<div class="directions-button-wrapper">
+		<a href="<?= $DIRECTIONS_URL ?>" class="cms-btn w-inline-block"><div class="button-icon"><i class="fa-solid fa-diamond-turn-right"></i></div><div class="button-text">Directions</div></a>
+	</div>
 
-	<? clr(); ?>
-	
 </div>
