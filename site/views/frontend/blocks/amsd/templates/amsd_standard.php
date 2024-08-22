18f92a1ad56e13e617403472e523fc9935a6a382
tyler-griffin <tyler.michael.griffin@gmail.com>
2024-08-22 15:39:12 -0500

    Update amsd_standard.php

--- site/views/frontend/blocks/amsd/templates/amsd_standard.php
+++ site/views/frontend/blocks/amsd/templates/amsd_standard.php
@@ -125,105 +125,109 @@ if($profile) {
 
 			<? foreach($amsd["data"] as $k => $CATEGORY) { ?>
 
-				<? if($CATEGORY->title) { ?>
-					<div class="heading category-title"><? if($CATEGORY->icon && $CATEGORY->icon != 'null') { ?><span class="category-icon"><i class="<?= $CATEGORY->icon ?>"></i></span> <? } ?><?= $CATEGORY->title ?></div>
-				<? } ?>
+				<? if(sizeof($CATEGORY->items) > 0) { ?>
 
-				<div class="amsd-category-wrapper <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
+					<? if($CATEGORY->title) { ?>
+						<div class="heading category-title"><? if($CATEGORY->icon && $CATEGORY->icon != 'null') { ?><span class="category-icon"><i class="<?= $CATEGORY->icon ?>"></i></span> <? } ?><?= $CATEGORY->title ?></div>
+					<? } ?>
 
-					<? foreach($CATEGORY->items as $k => $ITEM) { ?>
+					<div class="amsd-category-wrapper <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
 
-						<? // Generate the profile URL
-						$link = isset($block->settings["Slug"]) && strlen(strip_tags($ITEM->html)) > 2 ? amsdProfileSlug($page, $amsd, $ITEM) : '';
-						$link = isset($ITEM->url) ? $ITEM->url : $link; ?>
-					
-						<div class="amsd-item <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
+						<? foreach($CATEGORY->items as $k => $ITEM) { ?>
 
-							<? // Get image URL
-							$itemImageUrl = false;
-							$itemImagePosition = false;
-							if(isset($ITEM->focused_img)) {
-								$itemImage = json_decode($ITEM->focused_img);
-								$itemImagePosition = $itemImage->config->{'background-position'};
-								$itemImageUrl =  '/image/' . $itemImage->id . '/600';
-							} else if($block->settings["Table"] == "amsd_staff") {
-								$itemImageUrl = '/assets/images/staff-placeholder.jpg';
-							} ?>
+							<? // Generate the profile URL
+							$link = isset($block->settings["Slug"]) && strlen(strip_tags($ITEM->html)) > 2 ? amsdProfileSlug($page, $amsd, $ITEM) : '';
+							$link = isset($ITEM->url) ? $ITEM->url : $link; ?>
 						
-							<? if($itemImageUrl) { ?>
-								<a <? if($link) { ?>href="<?= $link ?>"<? } ?> class="amsd-image-link w-inline-block <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" title="<?= $ITEM->title ?>">
-									<div class="amsd-image <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" style="<? if($itemImagePosition) { ?>background-position: <?= $itemImagePosition ?>;<? } ?> background-image: url('<?= $itemImageUrl ?>');">
-										<? if($link) { ?><div class="hover-overlay"></div><? } ?>
-									</div>
-								</a>
-							<? } ?>
+							<div class="amsd-item <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
 
-							<div class="amsd-text-wrapper <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
-
-								<a <? if($link) { ?>href="<?= $link ?>"<? } ?> class="amsd-title-text-link <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $ITEM->title ?></a>
-
-								<? if(isset($ITEM->sub_title)) { ?>
-									<p class="amsd-meta-text <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $ITEM->sub_title ?></p>
+								<? // Get image URL
+								$itemImageUrl = false;
+								$itemImagePosition = false;
+								if(isset($ITEM->focused_img)) {
+									$itemImage = json_decode($ITEM->focused_img);
+									$itemImagePosition = $itemImage->config->{'background-position'};
+									$itemImageUrl =  '/image/' . $itemImage->id . '/600';
+								} else if($block->settings["Table"] == "amsd_staff") {
+									$itemImageUrl = '/assets/images/staff-placeholder.jpg';
+								} ?>
+							
+								<? if($itemImageUrl) { ?>
+									<a <? if($link) { ?>href="<?= $link ?>"<? } ?> class="amsd-image-link w-inline-block <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" title="<?= $ITEM->title ?>">
+										<div class="amsd-image <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" style="<? if($itemImagePosition) { ?>background-position: <?= $itemImagePosition ?>;<? } ?> background-image: url('<?= $itemImageUrl ?>');">
+											<? if($link) { ?><div class="hover-overlay"></div><? } ?>
+										</div>
+									</a>
 								<? } ?>
 
-								<? if(isset($ITEM->email) || isset($ITEM->phone)) { ?>
-									<div class="amsd-meta-links-outer-wrapper">
-										<? if(isset($ITEM->email)) { ?>
-											<div class="amsd-meta-link-wrapper">
-												<div class="amsd-meta-icon"><i class="fas fa-envelope"></i></div>
-												<a href="mailto:<?= $ITEM->email ?>" class="amsd-meta-link"><?= $ITEM->email ?></a>
-											</div>
-										<? } ?>
-										<? if(isset($ITEM->phone)) { ?>
-											<div class="amsd-meta-link-wrapper">
-												<div class="amsd-meta-icon"><i class="fas fa-phone-alt"></i></div>
-												<a href="tel:+1<?= $ITEM->phone ?>" class="amsd-meta-link"><?= $ITEM->phone ?></a>
-											</div>
-										<? } ?>
-									</div>
-								<? } ?>
+								<div class="amsd-text-wrapper <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>">
 
-								<? $itemPreviewText = false;
-								if($block->settings["Table"] != "amsd_staff") {
-									if($ITEM->preview_text) {
-										$itemPreviewText = nl2br($ITEM->preview_text);
-									} else if(strlen(strip_tags($ITEM->html)) > 2) {
-										$itemPreviewText = character_limiter(strip_tags($ITEM->html), 180);
+									<a <? if($link) { ?>href="<?= $link ?>"<? } ?> class="amsd-title-text-link <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $ITEM->title ?></a>
+
+									<? if(isset($ITEM->sub_title)) { ?>
+										<p class="amsd-meta-text <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $ITEM->sub_title ?></p>
+									<? } ?>
+
+									<? if(isset($ITEM->email) || isset($ITEM->phone)) { ?>
+										<div class="amsd-meta-links-outer-wrapper">
+											<? if(isset($ITEM->email)) { ?>
+												<div class="amsd-meta-link-wrapper">
+													<div class="amsd-meta-icon"><i class="fas fa-envelope"></i></div>
+													<a href="mailto:<?= $ITEM->email ?>" class="amsd-meta-link"><?= $ITEM->email ?></a>
+												</div>
+											<? } ?>
+											<? if(isset($ITEM->phone)) { ?>
+												<div class="amsd-meta-link-wrapper">
+													<div class="amsd-meta-icon"><i class="fas fa-phone-alt"></i></div>
+													<a href="tel:+1<?= $ITEM->phone ?>" class="amsd-meta-link"><?= $ITEM->phone ?></a>
+												</div>
+											<? } ?>
+										</div>
+									<? } ?>
+
+									<? $itemPreviewText = false;
+									if($block->settings["Table"] != "amsd_staff") {
+										if($ITEM->preview_text) {
+											$itemPreviewText = nl2br($ITEM->preview_text);
+										} else if(strlen(strip_tags($ITEM->html)) > 2) {
+											$itemPreviewText = character_limiter(strip_tags($ITEM->html), 180);
+										}
 									}
-								}
-								if($itemPreviewText) { ?>
-									<p class="amsd-description-text <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $itemPreviewText ?></p>
-								<? } ?>
+									if($itemPreviewText) { ?>
+										<p class="amsd-description-text <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>"><?= $itemPreviewText ?></p>
+									<? } ?>
 
-								<? if($link) { ?>
+									<? if($link) { ?>
 
-									<? $buttonText = '';
-									if(isset($ITEM->button_text)) {
-										$buttonText = $ITEM->button_text;
-									} else if($block->settings["Table"] == "amsd_staff") {
-										$buttonText = 'View Bio';
-									} else {
-										$buttonText = 'Learn More';
-									} ?>
+										<? $buttonText = '';
+										if(isset($ITEM->button_text)) {
+											$buttonText = $ITEM->button_text;
+										} else if($block->settings["Table"] == "amsd_staff") {
+											$buttonText = 'View Bio';
+										} else {
+											$buttonText = 'Learn More';
+										} ?>
 
-									<div class="amsd-button-wrapper">
-										<a href="<?= $link ?>" class="amsd-button <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" title="<?= $ITEM->title ?>"><?= $buttonText ?> <span class="amsd-button-arrow"><i class="fas fa-chevron-right"></i></span></a>
-									</div>
-									
-								<? } ?>
+										<div class="amsd-button-wrapper">
+											<a href="<?= $link ?>" class="amsd-button <?= $CLASS_GRID ?> <?= $CLASS_STAFF ?>" title="<?= $ITEM->title ?>"><?= $buttonText ?> <span class="amsd-button-arrow"><i class="fas fa-chevron-right"></i></span></a>
+										</div>
+										
+									<? } ?>
 
+								</div>
+							
 							</div>
-						
-						</div>
 
-					<? } ?>
+						<? } ?>
 
-					<? if($CLASS_GRID) { ?>
-						<div class="amsd-item grid remainder-items-fix"></div>
-						<div class="amsd-item grid remainder-items-fix"></div>
-					<? } ?>
+						<? if($CLASS_GRID) { ?>
+							<div class="amsd-item grid remainder-items-fix"></div>
+							<div class="amsd-item grid remainder-items-fix"></div>
+						<? } ?>
 
-				</div>
+					</div>
+
+				<? } ?>
 			
 			<? } ?>
 
