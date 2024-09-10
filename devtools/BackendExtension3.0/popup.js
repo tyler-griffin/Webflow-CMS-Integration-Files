25eb231ab11f95e7cb8732ab1db63e306b48433c
tyler-griffin <tyler.michael.griffin@gmail.com>
2024-09-10 16:46:12 -0500

    Added new tab functionality to backend extension

--- devtools/BackendExtension3.0/popup.js
+++ devtools/BackendExtension3.0/popup.js
@@ -1,4 +1,30 @@
-document.getElementById('addSnippet').addEventListener('click', function() {
+document.getElementById('updateCurrentTab').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+ 
+        let currentUrl = tabs[0].url;
+ 
+        currentUrl = currentUrl.replace("https://", "");
+        currentUrl = currentUrl.replace("http://", "");
+        currentUrl = currentUrl.replace(/\/+$/, "");
+ 
+        let urlParts = currentUrl.split('/');
+ 
+        let domain = urlParts[0];
+ 
+        urlParts.shift();
+ 
+        let path = urlParts.join("/");
+ 
+        let modifiedUrl = "https://" + domain + "/backend";
+ 
+        if(path != "") { modifiedUrl += "#/editor/" + path; }
+
+        chrome.tabs.update(tabs[0].id, {url: modifiedUrl});
+        
+    });
+});
+
+document.getElementById('openNewTab').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
  
         let currentUrl = tabs[0].url;
